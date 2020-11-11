136117793031c33d669931a0a589e19c788949f2
ambonetto <bonettoandream@johndeere.com>
2020-11-11 16:03:33 -0600

    Profile Container functionality setup

---
+++ shirts-frontend/src/components/MyAccount/ProfileContainer.js
@@ -0,0 +1,16 @@
+// import React
+import React from 'react';
+
+// import Update Profile.js
+
+// function base
+const ProfileContainer = (props) => {
+    return (
+        <div className="profileContainer">
+            <h1>My Account</h1>
+        </div>
+    )
+}
+
+// export My Account Container
+export default ProfileContainer;
