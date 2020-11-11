8975f9de9c5c874532d09355b7c9b0eabc59c066
ambonetto <bonettoandream@johndeere.com>
2020-11-11 16:07:07 -0600

    Basic Profile setup

---
+++ shirts-frontend/src/components/MyAccount/Profile.js
@@ -0,0 +1,21 @@
+// import React
+import React, {Component} from 'react';
+
+// class base
+//  displaying profile
+class Profile extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
+
+// export Profile
+export default Profile;
--- shirts-frontend/src/components/MyAccount/ProfileContainer.js
+++ shirts-frontend/src/components/MyAccount/ProfileContainer.js
@@ -1,13 +1,15 @@
 // import React
 import React from 'react';
 
-// import Update Profile.js
+// import Profile
+import Profile from './Profile';
 
 // function base
 const ProfileContainer = (props) => {
     return (
         <div className="profileContainer">
             <h1>My Account</h1>
+            <Profile />
         </div>
     )
 }
