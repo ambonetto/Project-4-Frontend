906283cc2f5e5e7aaaf33f5a5fab6995e9ed7a82
ambonetto <bonettoandream@johndeere.com>
2020-11-13 10:51:23 -0600

    Initial Setup

---
+++ shirts-frontend/src/components/Shirts/ShirtsContainer.js
@@ -0,0 +1,19 @@
+// import React 
+import React, {Component} from 'react';
+
+class ShirtsContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div className="container">
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
+
+// export Shirts Container
+export default ShirtsContainer;
