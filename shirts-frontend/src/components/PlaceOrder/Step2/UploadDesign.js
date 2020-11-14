4f899a225dd5734a5a5d7f488a7dc7d4bccec1de
ambonetto <bonettoandream@johndeere.com>
2020-11-13 22:02:21 -0600

    Working on URL Image Preview

--- shirts-frontend/src/components/PlaceOrder/Step2/UploadDesign.js
+++ shirts-frontend/src/components/PlaceOrder/Step2/UploadDesign.js
@@ -8,7 +8,8 @@ class UploadDesign extends Component {
 
         this.state = {
             design: props.design,
-            selectFile: null
+            selectFile: null,
+            URL: null
         }
     }
 
@@ -17,21 +18,46 @@ class UploadDesign extends Component {
     handleFileSelected = (e) => {
         console.log(e.target.files[0])
 
+        // this will give a preview of the image
         this.setState({
             selectFile: URL.createObjectURL(e.target.files[0])
         })
+        console.log(this.state.selectFile)
+    }
+
+    // handle file URL function
+    handleFileURL = (e) => {
+        const URL = this.state.URL
+
+        this.setState({
+            selectFile: URL
+        })
+
+        console.log(this.state.URL)
+        console.log(this.state.selectFile)
     }
 
     render() {
         return(
             <div className="container">
                 <h1>Upload a Custom Design</h1>
+                <h3>Upload from Computer</h3>
                 <input 
                     type="file"
                     onChange={this.handleFileSelected}
                 />
+                <h3>Upload from the Web</h3>
+                <input 
+                    type="text"
+                    value={this.state.URL}
+                    onChange={this.handleFileURL}
+                />
                 <img className="image" src={this.state.selectFile} />
+                <img className="image" src={this.state.URL} />
+                <br />
+                <input type="submit" value="Submit" />
                 <h6>Note: May be contacted if there is an issue with the design</h6>
+                
             </div>
         )
     }
