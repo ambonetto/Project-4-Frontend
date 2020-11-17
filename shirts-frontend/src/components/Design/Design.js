ff673f01af7163ddd96c98a89fbb77ac17528037
ambonetto <bonettoandream@johndeere.com>
2020-11-16 20:00:13 -0600

    Updating styling for Designs

--- shirts-frontend/src/components/Design/Design.js
+++ shirts-frontend/src/components/Design/Design.js
@@ -5,18 +5,20 @@ import React from 'react';
 //  displaying all designs
 const Design = (props) => {
     return(
-        <div className="container">
-            {props.allDesigns.map((design, index) => {
-                return <div key={index}>
-                    {!props.currentUser.admin === false ? 
-                        <button>Delete Design</button>
-                    :
-                        null 
-                    }
-                    <li>{design.name}</li>
-                    <img className="image" src={design.image} alt={design.name} />
-                </div>
-            })}
+        <div className="designContainer">
+            <div className="designWrapper">
+                {props.allDesigns.map((design, index) => {
+                    return <div key={index}>
+                        {!props.currentUser.admin === false ? 
+                            <button>Delete Design</button>
+                        :
+                            null 
+                        }
+                        <li>{design.name}</li>
+                        <img className="image" src={design.image} alt={design.name} />
+                    </div>
+                })}
+            </div>
         </div>
     )
 }
