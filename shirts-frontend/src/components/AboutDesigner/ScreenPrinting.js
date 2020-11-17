886b292ec68b56683aa75b1ebb49d05be34c27ce
ambonetto <bonettoandream@johndeere.com>
2020-11-17 13:05:29 -0600

    Updating styling

--- shirts-frontend/src/components/AboutDesigner/ScreenPrinting.js
+++ shirts-frontend/src/components/AboutDesigner/ScreenPrinting.js
@@ -6,12 +6,12 @@ const ScreenPrinting = () => {
         <div className="aboutScreenPrintingContainer">
             <div className="imgWrapper">
                 <img className="imageScreenPrinting" src="https://raw.githubusercontent.com/ambonetto/Project-4-Frontend/master/Images/ScreenPrinting.jpg?_sm_au_=iVVzMk7320SWq3tQL321jK0f1JH33" alt="Screen Printing" />
+                <br />
                 <span className="caption">Screen Printing</span>
             </div>
             <br />
             <div className="aboutScreenPrintingWrapper">
                 <h1>What is Screen Printing?</h1>
-                <br />
                 <p className="infoScreenPrinting">
                     Screen Printing, which is also known as Silk Screening, is a method of
                     printing graphics on a t-shirt using thick inks that lay on top of the shirt rather
@@ -26,6 +26,8 @@ const ScreenPrinting = () => {
                     where the design has been printed allows ink to slip through. The ink then sets on the t-shirt
                     and is put aside to dry.
                 </p>
+                <br />
+                <h5>Source: One Hour Tees</h5>
                 <a href="https://www.onehourtees.com/blog/what-is-screen-printing-how-does-it-work/" target="_blank">Continue Reading</a>
             </div>
             </div>
