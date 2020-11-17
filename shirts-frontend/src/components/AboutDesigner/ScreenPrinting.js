b2247c81d33e6fbbe715a1556f9f05a67d656825
ambonetto <bonettoandream@johndeere.com>
2020-11-17 12:59:05 -0600

    Updating About Designer Page

--- shirts-frontend/src/components/AboutDesigner/ScreenPrinting.js
+++ shirts-frontend/src/components/AboutDesigner/ScreenPrinting.js
@@ -3,22 +3,23 @@ import React from 'react';
 
 const ScreenPrinting = () => {
     return (
-        <div className="aboutMeContainer">
+        <div className="aboutScreenPrintingContainer">
             <div className="imgWrapper">
-                <img className="image" src="https://lh3.googleusercontent.com/proxy/1ScJDG202WwiifwKl8X8mjxRCQ0NP_nQJ3aDywp3Luylmg6c312CjprAm1HRwWvbQz2wr_HF1E8DPAjiZneT-SicPcaXjoL7W2ECzaI6x5t4p4XPoJU1QUWNqptLk5jASR8" alt="Screen Printing" />
+                <img className="imageScreenPrinting" src="https://raw.githubusercontent.com/ambonetto/Project-4-Frontend/master/Images/ScreenPrinting.jpg?_sm_au_=iVVzMk7320SWq3tQL321jK0f1JH33" alt="Screen Printing" />
                 <span className="caption">Screen Printing</span>
             </div>
-            <div className="aboutMeWrapper">
+            <br />
+            <div className="aboutScreenPrintingWrapper">
                 <h1>What is Screen Printing?</h1>
                 <br />
-                <p className="info">
+                <p className="infoScreenPrinting">
                     Screen Printing, which is also known as Silk Screening, is a method of
                     printing graphics on a t-shirt using thick inks that lay on top of the shirt rather
                     than soaking into the shirt. 
                 </p>
                 <br />
                 <h1>How Does it Work?</h1>
-                <p className="info">
+                <p className="infoScreenPrinting">
                     A thin mesh is stretched tightly over a frame (originally the screen was made from silk, 
                     however, mostly made from polyester today). A negative of the design is printed onto the screen
                     to be placed against the shirt. Once set correctly, ink is rolled over the screen, only the areas
